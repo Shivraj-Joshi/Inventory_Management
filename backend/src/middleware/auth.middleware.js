1a8898bf949727f8b6d7edf3d786755dc42ae0d3
Shivraj joshi <shibujoshi07@gmail.com>
2026-08-09 23:55:08 +0530

    build authentication middleware

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+
+const authenticate = (req, res, next) => {
+
+    const authHeader = req.headers.authorization
+
+
+
+    try {
+
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return res.status(401).json({ message: 'Token not found' })
+        }
+
+        const token = authHeader.split(' ')[1]
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        req.user = decoded
+        next()
+
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid Token !!' })
+    }
+
+
+}
